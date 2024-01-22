5fe1def26e7a48b1e33af6d849742c301550ac16
IF_Abhay ? <64267240+Abhay7apk@users.noreply.github.com>
2024-01-22 12:36:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    // Show the popup
+    document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+    // Close the popup
+    document.getElementById("popup").style.display = "none";
+}
